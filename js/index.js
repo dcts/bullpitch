d0118f37a80fb89e8504a3ee444bb9f61098c0cf
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-11 18:43:39 +0200

    restructure javascript code

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// random degree function (n = number of spins)
+const randomExtraSpins = n => Math.random()*n*360;
+
+// initialize degrees
+const minSpinDegrees = 4 * 360; // 6 spins minimum!
+let degreeSpinSum = minSpinDegrees + randomExtraSpins(3); // 3 extra spins max!
+
+// initialize DOM objects
+const button = document.getElementById("btn-spinner");
+const spinner = document.getElementById("spinner-img");
+const bullbar = document.getElementById("bull-bar");
+
+// ON BUTTON CLICK -> SPIN THE WHEEL!
+button.addEventListener('click', (event) => {
+  // reset
+  bullbar.setAttribute("style", "border: none;");
+  spinner.innerHTML = "Spin";
+  bullbar.innerHTML = "- - - - -";
+  // calculate spinner degrees and spin! (transformation)
+  degreeSpinSum += minSpinDegrees + randomExtraSpins(3);
+  const selectedBezier = selectBezier();
+  let buildStyle = `transform: rotate(${degreeSpinSum}deg); transition-timing-function: ${selectedBezier}`;
+  spinner.setAttribute("style", buildStyle);
+  // wait for spinner to end transition!
+  $("#spinner-img").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", () => {
+    bullbar.innerHTML = `${startupify()}`;
+    bullbar.setAttribute("style", "border: 5px solid #8BFFC7; transition: ease 0.25s;");
+    button.innerHTML = "Pitch";
+  });
+});
+
+
+
+/*
+ * SELECT BEZIER
+ * randomly picks one predefined bezier transition function
+ * resources used: https://codepen.io/Guilh/full/ZQxoOX/
+ */
+function selectBezier() {
+  let sampleArray = [
+    "cubic-bezier(0.6, 1.5, 0.6, 1)",
+    "cubic-bezier(.1, .9, .9, .1)",
+    "cubic-bezier(0.68, -0.55, 0.265, 1.55)",
+    "cubic-bezier(0.25, 0. 1, 0.25, 1)"
+  ];
+  return sampleArray[Math.floor(Math.random() * sampleArray.length)];
+}
--- js/startupify.js
+++ js/startupify.js
@@ -1,53 +1,3 @@
-// random degree function (n = number of spins)
-const randomExtraSpins = n => Math.random()*n*360;
-
-// initialize degrees
-const minSpinDegrees = 4 * 360; // 6 spins minimum!
-let degreeSpinSum = minSpinDegrees + randomExtraSpins(3); // 3 extra spins max!
-
-// initialize DOM objects
-const button = document.getElementById("btn-spinner");
-const spinner = document.getElementById("spinner-img");
-const bullbar = document.getElementById("bull-bar");
-
-// ON BUTTON CLICK -> SPIN THE WHEEL!
-button.addEventListener('click', (event) => {
-  // reset
-  bullbar.setAttribute("style", "border: none;");
-  spinner.innerHTML = "Spin";
-  bullbar.innerHTML = "- - - - -";
-  // calculate spinner degrees and spin! (transformation)
-  degreeSpinSum += minSpinDegrees + randomExtraSpins(3);
-  const selectedBezier = selectBezier();
-  let buildStyle = `transform: rotate(${degreeSpinSum}deg); transition-timing-function: ${selectedBezier}`;
-  spinner.setAttribute("style", buildStyle);
-  // wait for spinner transition to end
-  $("#spinner-img").bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", () => {
-    bullbar.innerHTML = `${startupify()}` ; //setAttribute("class", "bigfont");
-    bullbar.setAttribute("style", "border: 5px solid #8BFFC7; transition: ease 0.25s;");
-    button.innerHTML = "Pitch" ; //setAttribute("class", "bigfont");
-  });
-});
-
-
-
-/*
- * SELECT BEZIER
- * randomly picks one predefined bezier transition function
- * resources used: https://codepen.io/Guilh/full/ZQxoOX/
- */
-function selectBezier() {
-  let sampleArray = [
-    "cubic-bezier(0.6, 1.5, 0.6, 1)",
-    "cubic-bezier(.1, .9, .9, .1)",
-    "cubic-bezier(0.68, -0.55, 0.265, 1.55)",
-    "cubic-bezier(0.25, 0. 1, 0.25, 1)"
-  ];
-  return sampleArray[Math.floor(Math.random() * sampleArray.length)];
-}
-
-
-
 /*
  * STARTUPIFY
  * first 600 names scraped from https://tiffzhang.com/startup and saved in an array
@@ -588,10 +538,3 @@ function startupify() {
   ];
   return sampleArray[Math.floor(Math.random() * sampleArray.length)];
 }
-
-// "SCRAPE" startupify code
-// let arr = [];
-// for (let i=0; i<12000; i++) {
-//   arr.push(startupify(i));
-// }
-// console.log(arr);
